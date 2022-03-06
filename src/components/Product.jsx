4aeac3c0610f54055e6cb70d297feb2bacc43803
ABetancourt03 <neutralzgamer@gmail.com>
2022-03-06 00:03:52 -0430

    Agregando Componente de Producto

---
+++ src/components/Product.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className='product'>
+      <img src="https://http2.mlstatic.com/D_Q_NP_905176-MLV43506956032_092020-AB.webp" alt="Product" />
+      <div className='price'>
+        <h2>U$S 50.79</h2>
+        <p className='shipping'>Envío gratis</p>
+      </div>
+    </div>
+  )
+}
+
+export default Product
