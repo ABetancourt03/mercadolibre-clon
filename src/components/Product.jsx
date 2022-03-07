6354a47b30d4231a000440fbdce7ccabaa4cc236
ABetancourt03 <neutralzgamer@gmail.com>
2022-03-06 19:33:47 -0430

    Volviendo mas dinamico el renderizado de Productos

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Product = () => {
+const Product = ({ img, title, price, currency, shipping }) => {
   return (
     <div className="product">
-      <img
-        src="https://http2.mlstatic.com/D_Q_NP_905176-MLV43506956032_092020-AB.webp"
-        alt="Product"
-      />
+      <img src={img} alt="Product" />
       <div className="price">
-        <h2>U$S 69.99</h2>
-        <p className="shipping">Envío gratis</p>
+        <h2>
+          {currency} {price}
+        </h2>
+        {shipping === 'free' && <p className="shipping">Envío gratis</p>}
+        <p className="product-title">{title}</p>
       </div>
     </div>
   )
