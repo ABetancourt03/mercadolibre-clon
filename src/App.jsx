ac976a744c2e50a2d572927206644f298863b19c
ABetancourt03 <neutralzgamer@gmail.com>
2022-03-07 10:39:17 -0430

    Agregando detalles de producto con su ruta

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // Components
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import ProductDetails from './components/ProductDetails'
+
+const Details1 = {
+  image:
+    'https://http2.mlstatic.com/D_Q_NP_905176-MLV43506956032_092020-AB.webp',
+  title: 'Teléfono Android Celular Krip K4m 3g 1gb Ram (sin Cargador)',
+  price: '69.99',
+  currency: '$',
+  shipping: '',
+  quantity: '50'
+}
 
 const App = () => {
   return (
@@ -11,7 +22,11 @@ const App = () => {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/products/product1"></Route>
+          <Route
+            exact
+            path="/products/product1"
+            children={<ProductDetails details={Details1} />}
+          />
         </Switch>
       </Router>
     </div>
