412dc847ba94d078ae96ea0325d4483cb86a956f
ABetancourt03 <neutralzgamer@gmail.com>
2022-03-06 19:34:10 -0430

    Agregando Productos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+
+// Styles
 import '../styles/Home.css'
+
+// Components
 import Product from './Product'
+
+// Styles
+import '../styles/Home.css'
 
 const Home = () => {
   return (
@@ -18,10 +25,73 @@ const Home = () => {
       </div>
       <div className="home">
         <div className="products">
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            img={
+              'https://http2.mlstatic.com/D_Q_NP_905176-MLV43506956032_092020-AB.webp'
+            }
+            title={
+              'Teléfono Android Celular Krip K4m 3g 1gb Ram (sin Cargador)'
+            }
+            price={'69.99'}
+            currency={'$'}
+            shipping={''}
+          />
+          <Product
+            img={
+              'https://http2.mlstatic.com/D_Q_NP_662952-MLV32048276000_092019-AB.webp'
+            }
+            price={'56.99'}
+            currency={'$'}
+            shipping={'free'}
+          />
+          <Product
+            img={
+              'https://http2.mlstatic.com/D_Q_NP_892986-MLV31248930335_062019-AB.webp'
+            }
+            currency={'$'}
+            price={'15.99'}
+            shipping={'free'}
+          />
+          <Product
+            img={
+              'https://http2.mlstatic.com/D_Q_NP_704175-MLV46485100423_062021-AB.webp'
+            }
+            price={'99.99'}
+            currency={'$'}
+            shipping={''}
+          />
+          <Product
+            img={
+              'https://http2.mlstatic.com/D_Q_NP_905176-MLV43506956032_092020-AB.webp'
+            }
+            price={'69.99'}
+            currency={'$'}
+            shipping={''}
+          />
+          <Product
+            img={
+              'https://http2.mlstatic.com/D_Q_NP_662952-MLV32048276000_092019-AB.webp'
+            }
+            price={'56.99'}
+            currency={'$'}
+            shipping={'free'}
+          />
+          <Product
+            img={
+              'https://http2.mlstatic.com/D_Q_NP_892986-MLV31248930335_062019-AB.webp'
+            }
+            currency={'$'}
+            price={'15.99'}
+            shipping={'free'}
+          />
+          <Product
+            img={
+              'https://http2.mlstatic.com/D_Q_NP_704175-MLV46485100423_062021-AB.webp'
+            }
+            price={'99.99'}
+            currency={'$'}
+            shipping={''}
+          />
         </div>
       </div>
     </div>
