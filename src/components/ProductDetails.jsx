d67bf911300547347591b50e0540d4192229d3d8
ABetancourt03 <neutralzgamer@gmail.com>
2022-03-07 16:04:00 -0430

    Agregando detalles del producto

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,22 +2,39 @@ import React from 'react'
 import '../styles/ProductDetails.css'
 
 const ProductDetails = ({ details }) => {
-  let { image, title, price, currency, shipping, quantity } = details
+  let { image, title, price, currency, shipping, location, quantity } = details
 
   return (
     <div className="details-container">
       <div className="product-details">
-        <img src={image} alt="Product Image" />
-        <h2>{title}</h2>
-        <p className="price">
-          {currency} {price}
-        </p>
-        {shipping === 'true' && <p className="shipping">Envío gratis</p>}
-        <p className="units">
-          {quantity > 1 && `${quantity} unidades disponibles`}
-          {quantity === '0' && `${quantity} unidades disponibles`}
-          {quantity === '1' && `${quantity} unidad disponible`}
-        </p>
+        <div className="product-images">
+          <div className="image"></div>
+          <div className="image"></div>
+          <div className="image"></div>
+        </div>
+
+        <div className="product-image">
+          <img src={image} alt="Product Image" />
+        </div>
+
+        <div className="product-info">
+          <h2 className="product-title">{title}</h2>
+          <p className="product-price">
+            {currency} {price}
+          </p>
+          {shipping === 'free' && (
+            <p className="shipping">Envío gratis a todo el país</p>
+          )}
+          <p className="location">{location}</p>
+          <p className="units">
+            {quantity > 1 && `${quantity} unidades disponibles`}
+            {quantity === '0' && `${quantity} unidades disponibles`}
+            {quantity === '1' && `${quantity} unidad disponible`}
+          </p>
+          <div className="buy">
+            <button className="buy-btn">Comprar Ahora</button>
+          </div>
+        </div>
       </div>
     </div>
   )
