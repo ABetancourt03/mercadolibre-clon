ad71a09371de70258e58c1e2cf99d6e0f454b8df
ABetancourt03 <neutralzgamer@gmail.com>
2022-03-07 21:36:55 -0430

    Agregando imagenes del producto y estilos

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,16 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/ProductDetails.css'
 
+const initialImage =
+  'https://http2.mlstatic.com/D_Q_NP_905176-MLV43506956032_092020-AB.webp'
+
 const ProductDetails = ({ details }) => {
-  let { image, title, price, currency, shipping, location, quantity } = details
+  let { title, price, currency, shipping, location, quantity } = details
+  const [image, setImage] = useState(initialImage)
+
+  const handleChangeImage = (e) => {
+    if (e.target.id === 'image1') {
+      setImage(
+        'https://http2.mlstatic.com/D_Q_NP_905176-MLV43506956032_092020-AB.webp'
+      )
+    } else if (e.target.id === 'image2') {
+      setImage(
+        'https://http2.mlstatic.com/D_NQ_NP_740232-MLV43506953124_092020-O.webp'
+      )
+    } else if (e.target.id === 'image3') {
+      setImage(
+        'https://http2.mlstatic.com/D_NQ_NP_624629-MLV43506945617_092020-O.webp'
+      )
+    }
+  }
 
   return (
     <div className="details-container">
       <div className="product-details">
         <div className="product-images">
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
+          <div className="image">
+            <img
+              onClick={handleChangeImage}
+              id="image1"
+              width="50px"
+              src="https://http2.mlstatic.com/D_Q_NP_905176-MLV43506956032_092020-AB.webp"
+              alt="Image 1"
+            />
+          </div>
+          <div className="image">
+            <img
+              onClick={handleChangeImage}
+              id="image2"
+              width="50px"
+              src="https://http2.mlstatic.com/D_NQ_NP_740232-MLV43506953124_092020-O.webp"
+              alt="Image 2"
+            />
+          </div>
+          <div className="image">
+            <img
+              onClick={handleChangeImage}
+              id="image3"
+              width="50px"
+              src="https://http2.mlstatic.com/D_NQ_NP_624629-MLV43506945617_092020-O.webp"
+              alt="Image 3"
+            />
+          </div>
         </div>
 
         <div className="product-image">
